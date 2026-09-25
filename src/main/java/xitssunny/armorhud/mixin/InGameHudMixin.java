e50c597cc9910955640c4a8e08cb62a47b6b5f70
xItsSunny <luk.kliem@gmail.com>
2026-09-25 14:03:45 +0200

    added oneconfig and fixed rendering issues in hud and game

--- src/main/java/xitssunny/armorhud/mixin/InGameHudMixin.java
+++ src/main/java/xitssunny/armorhud/mixin/InGameHudMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiIngame;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,9 +26,12 @@ public abstract class InGameHudMixin {
 		int startY = screenHeight - 19;
 
 		GlStateManager.pushMatrix();
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 		GlStateManager.enableRescaleNormal();
 		GlStateManager.enableBlend();
 		GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+
+		RenderHelper.enableGUIStandardItemLighting();
 
 		int renderIndex = 0;
 		for (int i = 3; i >= 0; i--) {
@@ -44,8 +48,12 @@ public abstract class InGameHudMixin {
 			}
 		}
 
+		RenderHelper.disableStandardItemLighting();
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 		GlStateManager.disableRescaleNormal();
 		GlStateManager.disableBlend();
+		GlStateManager.enableDepth();
+		GlStateManager.disableLighting();
 		GlStateManager.popMatrix();
 	}
 }
